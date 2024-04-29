ac8b50bfbd8b1caa8cd006e9adf7a314b2f4294d
lef237 <lef237k5@gmail.com>
2024-04-29 10:27:53 +0900

    Implement the auto_move feature

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 
 var speed = 400
 var angular_speed = PI
-
+var auto_move = false
 
 func _process(delta):
 	var direction = 0
@@ -12,9 +12,15 @@ func _process(delta):
 		direction = 1
 
 	rotation += angular_speed * direction * delta
+	
+	if Input.is_action_just_pressed("ui_down"):
+		rotation += PI
 
 	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
+	if auto_move or Input.is_action_pressed("ui_up"):
 		velocity = Vector2.UP.rotated(rotation) * speed
 
 	position += velocity * delta
+	
+func _on_button_pressed():
+	auto_move = not auto_move
